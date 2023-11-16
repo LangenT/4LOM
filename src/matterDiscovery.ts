015b918f3afd1a7bd1b828166594f5fa8ba5b293
Till Langen <till-felix.langen@telekom.de>
2023-11-16 16:44:29 +0100

    discover every endpoint of a node instead of just the first one

--- src/matterDiscovery.ts
+++ src/matterDiscovery.ts
@@ -2,14 +2,14 @@ import axios from 'axios';
 import { smartPlug } from './thingTypes/smartPlug';
 import { baseUrl } from '..';
 
-export type discoveredNodeSchema = {
+export type discoveredEndpointSchema = {
   ETI: WoT.ExposedThingInit, //the Exposed Thing Init
-  deviceType: number //the device type
+  deviceType: number //the device type of the Exposed Thing
 }
 
-export const discoverNode = async (): Promise<discoveredNodeSchema[]> => {
-    let things: {ETI: WoT.ExposedThingInit, deviceType: number
-}[] = [];
+export const discoverNode = async (): Promise<discoveredEndpointSchema[]> => {
+  let things: discoveredEndpointSchema[] = [];
+
   let config = {
     method: 'post',
     maxBodyLength: Infinity,
@@ -32,12 +32,12 @@ export const discoverNode = async (): Promise<discoveredNodeSchema[]> => {
 
   //go through every node
   for (const nodeId of nodeIds) {
-    await getThingInit(nodeId).then((inst) => {
-        if (inst) {
-            const parseDeviceType = inst.title; // parse device Type
-            inst.title = nodeId.toString();
-            things.push({ ETI: inst, deviceType: Number(parseDeviceType) });
-      }
+    await getThingInit(nodeId).then((endpointSchemaArray) => {
+      endpointSchemaArray?.forEach(endpointSchema => {//go through every endpoint
+        if (endpointSchema) {
+          things.push(endpointSchema);//add every endpoint to the things
+    }
+      });
     });
   }
 
@@ -48,7 +48,7 @@ export const discoverNode = async (): Promise<discoveredNodeSchema[]> => {
 //give a nodeId and get the matching ExposedThingInit for the device Type
 const getThingInit = async (
   nodeId: number
-): Promise<WoT.ExposedThingInit | null> => {
+): Promise<discoveredEndpointSchema[] | null> => {
   let config = {
     method: 'post',
     maxBodyLength: Infinity,
@@ -71,24 +71,38 @@ const getThingInit = async (
 
   //currently the thing can only implement one device type!
   //TODO handle more than one device type from one node
-  if (
-    nodeDescription['0']['1']['Descriptor::DeviceTypeListA'][0]['deviceType']
-  ) {
-    return getDeviceType(
-      nodeDescription['0']['1']['Descriptor::DeviceTypeListA'][0]['deviceType']
-    );
-  } else {
-    return null;
+
+  const endpointSchemaArray: discoveredEndpointSchema[] = [];
+
+  //iterate trough every endpoint
+  let i = 1;
+  while (true) {
+    if (
+      nodeDescription['0'][i.toString()]['Descriptor::DeviceTypeListA'][0]['deviceType']
+    ) {
+      const endpointSchema = getDeviceType(
+        nodeDescription['0'][i.toString()]['Descriptor::DeviceTypeListA'][0]['deviceType']
+      );
+      if (endpointSchema) {
+        endpointSchemaArray.push(endpointSchema);
+      }
+    } else {
+      break;
+    }
+    i++;
   }
+
+  return endpointSchemaArray;
 };
 
 //get the ExposedThingInit to a given device type number (dec)
-const getDeviceType = (deviceType: number): WoT.ExposedThingInit | null => {
+const getDeviceType = (deviceType: number): discoveredEndpointSchema | null => {
   switch (deviceType) {
-      case 266:
+      case 266: //type smartPlug
         let inst = smartPlug;
-        inst.title = deviceType.toString();
-        return smartPlug;
+      inst.title = deviceType.toString() + "-" + "266";
+      console.log("found a new device: " + inst.title);
+      return { ETI: smartPlug, deviceType: 266 };
       default:
         console.log("unknown or not supported device type: " + deviceType);
         return null;
