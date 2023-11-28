bb61933b7a8b7799453efc1e3266aca6f7219fce
Till Langen <till-felix.langen@telekom.de>
2023-11-28 10:32:23 +0100

    add file for color light

---
+++ src/thingTypes/colorTemperatureLight.ts
@@ -0,0 +1,12 @@
+export const colorTemperatureLight: WoT.ExposedThingInit = {
+  title: 'colorTemperatureLight',
+  description:
+    'Color Temperature Light is a lighting device that is capable of ' +
+    'being switched on or off, the intensity of its light adjusted, and its color temperature adjusted',
+  properties: {
+    status: {
+    }
+  },
+  actions: {
+  }
+};
