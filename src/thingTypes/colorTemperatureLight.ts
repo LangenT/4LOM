a3443421d60740d53b439f5c8c79b989b67724b8
Till Langen <till-felix.langen@telekom.de>
2023-12-04 14:52:22 +0100

    use defined cluster structure from cluster files with spread operator instead of redundant code

--- src/thingTypes/colorTemperatureLight.ts
+++ src/thingTypes/colorTemperatureLight.ts
@@ -1,10 +1,22 @@
+import { actions0x0006, properties0x0006 } from '../cluster/0x0006';
+import { actions0x0008, properties0x0008 } from '../cluster/0x0008';
+import { actions0x0300, properties0x0300 } from '../cluster/0x0300';
+
 export const colorTemperatureLight: WoT.ExposedThingInit = {
   title: 'colorTemperatureLight',
   description:
     'Color Temperature Light is a lighting device that is capable of ' +
     'being switched on or off, the intensity of its light adjusted, and its color temperature adjusted',
   properties: {
-    status: {}
+    ...properties0x0006,//On/Off
+    ...properties0x0008,//Level Control
+    ...properties0x0300//Color Control
   },
-  actions: {}
+  actions: {
+    ...actions0x0006,//On/Off
+    ...actions0x0008,//Level Control
+    ...actions0x0300//Color Control
+  }
 };
+
+colorTemperatureLight.properties
